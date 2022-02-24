e0594a13be397011598cacd6078f39d508b5b038
Daniel Fischer <daniel.fischer281@gmail.com>
2022-02-24 12:52:11 +0100

    change Charts

--- R/BMA_Charts/BMA_Charts.R
+++ R/BMA_Charts/BMA_Charts.R
@@ -3,11 +3,14 @@ install.packages("dplyr")
 install.packages("patchwork")
 install.packages("hrbrthemes")
 install.packages("ggpubr")
+install.packages("data.table")
 library(ggplot2)
 library(dplyr)
 library(patchwork)
 library(hrbrthemes)
 library(ggpubr)
+library("data.table")
+fread(endcoding = "UTF-8")
 
 
 cf <- 35
@@ -34,7 +37,7 @@ ggplot(SQL_File_Name, aes(x=V1, group = 1)) +
 # -----------------------------------------------------------------------------
 
 
-# Anteil von I/P lÃ¤ufen Daniel
+# Anteil von I/P läufen Daniel
 
 dataD <- data.frame(
   Legende=c("E", "I/P"),
@@ -49,20 +52,20 @@ dataY <- data.frame(
 ch1 <- ggplot(dataD, aes(x="", y=values, fill=Legende))+
   geom_bar(stat="identity", width=1, color="white")+
   coord_polar("y", start=0)+
-  labs(title="Anteil I/P-LÃ¤ufe Daniel", fill="Lauftyp")+
+  labs(title="Anteil I/P-Läufe Daniel", fill="Lauftyp")+
   theme_void()
 
 ch2 <- ggplot(dataY, aes(x="", y=values, fill=Legende))+
   geom_bar(stat="identity", width=1, color="white")+
   coord_polar("y", start=0)+
-  labs(title="Anteil I/P-LÃ¤ufe Yanick", fill="Lauftyp")+
+  labs(title="Anteil I/P-Läufe Yanick", fill="Lauftyp")+
   theme_void()
 
-ch1 + ch2
+#ch1 + ch2
 
 
 # End Avg. Pace + Avg. Pace, Cadence and Distance
-AvgPace_End_Daniel$V1 <- format(as.Date(AvgPace_End_Daniel$V1), "%m-%d")
+#AvgPace_End_Daniel$V1 <- format(as.Date(AvgPace_End_Daniel$V1), "%m-%d")
 
 ch3 <- ggplot(AvgPace_End_Daniel, aes(V1, V2, group = 1)) +
   geom_line(size=2, color="#292f36") +
@@ -80,11 +83,11 @@ ch4 <- ggplot(End_Daniel_All, aes(x=V1,colour=V3, group = 1)) +
   geom_point(aes(y=V6, color=V3, group = 1), size = 4) +
   scale_color_gradient(low = "#ffdf40", high = "#c40000") +
   geom_smooth(aes(y=V6),method = lm) +
-  labs(x="Datum", y="Durchschnittliche Kadenz (tÃ¼rkis), Durchschnittliche Herzfrequenz (grau)",
+  labs(x="Datum", y="Durchschnittliche Kadenz (türkis), Durchschnittliche Herzfrequenz (grau)",
        color="Distanz", title="Durchschnittliche Kadenz und Herzfrequenz pro Ausdauerlauf")+
   theme_bw()
 
-ch3 + ch4
+#ch3 + ch4
 
 
 
@@ -98,7 +101,7 @@ ch5 <- ggplot(AvgPace_Int_All, aes(x=V2, y=V3, colour=V1, group=V1)) +
   geom_line(size=2) +
   geom_point() +
   labs(title ="Durchschnittsgeschwindigkeit (min/km) pro Teilinterval", x ="Datum", 
-       y="Durchschnittsgeschwindigkeit (min/km)", col="LÃ¤ufer")+
+       y="Durchschnittsgeschwindigkeit (min/km)", col="Läufer")+
   theme_bw()
 
 Int_StartEnd_All <- data.frame(rbind(cbind(Int_StartEnd_Daniel, V10 = "Daniel"), 
@@ -108,11 +111,11 @@ Int_StartEnd_All$V1 <- format(as.Date(Int_StartEnd_All$V1), "%m-%d")
 ch6 <- ggplot(Int_StartEnd_All, aes(fill=V1, x=V10, y=V8))+
   geom_bar(position="dodge", stat="identity")+
   coord_cartesian(ylim = c(150, NA))+
-  labs(title ="Maximale Durchschnittskadenz", x ="LÃ¤ufer", y="Kadenz", fill="Datum")+
+  labs(title ="Maximale Durchschnittskadenz", x ="Läufer", y="Kadenz", fill="Datum")+
   theme_bw()
 
 
-ggarrange(ch5, ch6, widths=c(2,1))
+#ch5 + ch6
 
 # Durchschnittspace 1. Lauf und 2. Lauf der Woche
 
@@ -122,7 +125,7 @@ ch7 <- ggplot(AvgPace_End_FiSec_Daniel, aes(fill=V4, x=V1, y=V2))+
   labs(title ="Durchschnitt der Durchschnittsgeschwindigkeit (min/km) 1. und 2. Ausdauerlauf pro Woche", x="Ausdauerlauf der Woche", y="Durchschnittsgeschwindigkeit (min/km)", fill="Kadenz")+
   theme_bw()
 
-ch7
+#ch7
 
 
 # Start-End Pace lauf Daniel/ Yanick
@@ -135,18 +138,12 @@ Pace_StartEnd_All$V2 <- as.POSIXct(Pace_StartEnd_All$V2, format="%H:%M:%S")
 
 ch8 <- ggplot(Pace_StartEnd_All, aes(x=V1, y=V2, group = V7, color=V7)) +
   geom_line(size=2) +
-  labs(title ="Start- und Endlauf auf 3km", x ="Datum", y="Laufzeit (min)", color="LÃ¤ufer")+
+  labs(title ="Start- und Endlauf auf 3km", x ="Datum", y="Laufzeit (min)", color="Läufer")+
   theme_bw()
 
-ch8
+#ch8
 
+ggarrange(ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, nrow = 4, ncol = 2, widths =rbind(c(1, 2), c(2, 1), c(1, 2), c(2, 1)))
 
-Avg_HeartRate_All <- data.frame("78  80	76	75	69	64	60
-                                      80	75	70	77	66	75	77
-                                      81	73	74	74	70	73	80
-                                      78	81	67	66	72	66	
-                                      76	70	70	64	78	76	
-                                      81	69	68	70	81	74	
-                                      77	68	73	81	75	70")
                                                                    
                                         
